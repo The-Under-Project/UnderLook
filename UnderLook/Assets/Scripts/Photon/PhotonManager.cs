a97596c80c2589ed52e6b3e688706c3ad097730b
root <root@DESKTOP-84URQF0.localdomain>
2020-01-20 21:36:06 +0100

    Photon setup

---
+++ UnderLook/Assets/Scripts/Photon/PhotonManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, TypeLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhontonNetwork.instantiate("Player", transform.position, Quaternion.identity);
+    }
+}
---
+++ UnderLook/Assets/Scripts/Photon/PlayerNetWorking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerNetWorking : MonoBehaviour
+{
+    public ManoBehaviour[] scriptsToIgnore;
+
+    private PhotonView photonView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        if (!photonView.IsMine)
+        {
+            foreach(var script in scriptsToIgnore)
+            {
+                script.enabled = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
