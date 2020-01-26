f5e0ad073b2712500ade30b783e5d7926f09a98b
Julien <julien.nicolle@epita.fr>
2020-01-26 17:57:02 +0100

    add Moving and CameraController scripts

---
+++ UnderLook/Assets/Scripts/Movement/Moving.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    CharacterController characterController;
+
+    [SerializeField] float speed, gravity, jumpspeed;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        
+        
+    }
+
+    void Update()
+    {
+        MovementPlayer();
+    }
+    void MovementPlayer()
+    {
+        float moveX = Input.GetAxis("Vertical") *  speed*Time.deltaTime;
+        float moveY = Input.GetAxis("Horizontal") *  speed*Time.deltaTime;
+        float moveZ = 0.0f;
+        
+        if ((Input.GetButton ("Jump") && characterController.isGrounded))
+        {
+            moveZ = jumpspeed;
+        }
+        moveZ -= gravity * Time.deltaTime;
+        
+        characterController.Move(transform.up * moveZ * Time.deltaTime + transform.forward*moveX + transform.right*moveY);
+        
+    }
+    
+
+}
