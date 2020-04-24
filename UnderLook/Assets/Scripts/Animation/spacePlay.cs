ef9ad81abd750ee4522a32e175904aba8628a952
Naexys <34689945+Naexys@users.noreply.github.com>
2020-04-24 18:59:19 +0200

    s

---
+++ UnderLook/Assets/Scripts/Animation/spacePlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spacePlay : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("space", !anim.GetBool("space"));
+        }
+    }
+}
