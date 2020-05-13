e661836300d7457c57c726d09a0bb0eabe548358
Paul LEGE <paul.lege@epita.fr>
2020-05-13 19:05:23 +0200

    Tashe

---
+++ UnderLook/Assets/Scripts/Character/Role/Heroes/Dps/Tashe/Tashe.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Player
+{
+    public class Tashe: Dps
+    {
+        [Header("Tashe")]
+
+        public GameObject canvasUI;
+        public Camera cam;
+        public GameObject sphere;
+
+        private Boolean isInUlt = false;
+
+
+
+
+        private void Start()
+        {
+
+            this.GetComponent<Moving>().speed = speed;
+            this.GetComponent<Moving>().jumpspeed = jumpspeed;
+
+            cam = GetComponentInChildren<Camera>();
+
+            canvasUI.GetComponent<UI>().hasShield = false;
+            canvasUI.GetComponent<UI>().hasThreeCapacities = false;
+            canvasUI.GetComponent<UI>().maxHP = hpmax;
+
+        }
+        void FixedUpdate()
+        {
+
+            if (hp <= 0)
+            {
+                //Debug.Log("Dead");
+                //dead();
+            }
+
+            canvasUI.GetComponent<UI>().CurrentHP = hp;
+        }
+
+        //-----------------------------
+        private void Update()
+        {
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                M1();
+            }
+            if (Input.GetButtonDown("Fire2"))
+            {
+                M2();
+            }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Cap1();
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Cap2();
+            }
+
+        }
+
+
+        private void M1()
+        {
+            if (!isInUlt)
+            {
+                //met le tire ici
+            }
+            else if (isInUlt)
+            {
+                
+            }
+        }
+        private void M2()
+        {
+            
+        }
+
+        private void Cap1()
+        {
+  
+        }
+
+        private void Cap2()
+        {
+            
+        }
+
+        private void Ulti()
+        {
+            
+        }
+    }
+}
